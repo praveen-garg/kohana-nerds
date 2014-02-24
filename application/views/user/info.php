806288da075f9e00deafd205ab9f225ba069858a
Praveen Garg <praveen.garg@nerdapplabs.com>
2014-02-24 16:49:41 +0530

    added bootstrap theme with Kohana::Form for user login and info views

--- application/views/user/info.php
+++ application/views/user/info.php
@@ -1,9 +1,10 @@
-<h2>Info for  user "<?= $user->username; ?>"</h2>
-
-<ul>
-	<li>Email: <?= $user->email; ?></li>
-	<li>Number of logins: <?= $user->logins; ?></li>
-	<li>Last Login: <?= Date::fuzzy_span($user->last_login); ?></li>
-</ul>
-
-<?= HTML::anchor('user/logout', 'Logout'); ?>
+<div class="bs-callout bs-callout-info">
+	<h4>User Info</h4>
+</div>
+You are logged in as : <kbd> "<?= $user->username;?>".</kbd> &nbsp; <?= HTML::anchor('user/logout', 'Logout');?>
+<br/>
+<p class="lead">
+	Email: <?= $user->email; ?> <br/>
+	Number of logins: <?= $user->logins; ?><br/>
+	Last Login: <?= Date::fuzzy_span($user->last_login); ?><br/>
+</p>
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,24 +1,31 @@
-<h2>Login</h2>
+<div class="bs-callout bs-callout-info"><h4>Login</h4></div>
 <? if ($message) : ?>
-	<h3 class="message">
+	<h3 class="message alert alert-info alert-dismissable">
+  		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 		<?= $message; ?>
 	</h3>
 <? endif; ?>
 
-<?= Form::open('user/login'); ?>
-
+<?= Form::open('user/login', array('role' => 'form')); ?>
+<div class="form-group">
 <?= Form::label('username', 'Username'); ?>
-<?= Form::input('username', HTML::chars(Arr::get($_POST, 'username'))); ?>
+<?= Form::input('username', HTML::chars(Arr::get($_POST, 'username')), array('placeholder' => 'username', 'class' => 'form-control')); ?>
+</div>
 
+<div class="form-group">
 <?= Form::label('password', 'Password'); ?>
-<?= Form::password('password'); ?>
+<?= Form::password('password', '',  array('class' => 'form-control')); ?>
+</div>
 
+<div class="form-group">
 <?= Form::label('remember', 'Remember Me'); ?>
-<?= Form::checkbox('remember'); ?>
+&nbsp;
+<?= Form::checkbox('remember', '', FALSE); ?>
+<p class="help-block">(Remember Me keeps you logged in for 2 weeks)</p>
+</div>
 
-<p>(Remember Me keeps you logged in for 2 weeks)</p>
-
-<?= Form::submit('login', 'Login'); ?>
+<div class="form-group">
+<?= Form::submit('login', 'Login', array('class' => 'btn btn-success')); ?>
 <?= Form::close(); ?>
-
-<p>Or <?= HTML::anchor('user/create', 'create a new account'); ?></p>
+&nbsp; Or <?= HTML::anchor('user/create', 'create a new account'); ?>
+</div>
